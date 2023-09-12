d6c7e425fdc7dd9b4f8a68a57d1e93891b1db129
George <yosho2mendoza@gmail.com>
2023-09-12 14:05:11 -0500

    POST DE ENCENDIDO

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ const cors = require('cors');
 const app = express();
 let mqttData = null;
 
+// Configuración de CORS
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:4200', // Cambia esto a tu origen permitido
   optionsSuccessStatus: 200,
 };
+
+app.use(cors(corsOptions)); // Habilita CORS para toda la aplicación
+app.options('*', cors(corsOptions)); // Configura las opciones CORS para permitir preflight requests
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
@@ -41,15 +46,19 @@ mqttClient.on('error', (error) => {
 //     res.json({ data });
 //   }
 // });
-mqttClient.subscribe('/pruebas', { qos: 0});
+const mqttSus = mqttClient.subscribe('test/server/channel_A', { qos: 2});
+
+const mqttCon = mqttClient.subscribe('test/server', { qos: 2 });
 
 // Manejar mensajes MQTT cuando se reciban
-mqttClient.on('message', (topic, message) => {
-  if (topic === '/pruebas') {
+mqttSus.on('message', (topic, message) => {
+  if (topic === 'test/server/channel_A') {
     mqttData = message.toString();
     console.log('Mensajes: ',mqttData);
   }
 });
+
+
 
 // Ruta de ejemplo para obtener datos MQTT como respuesta JSON
 app.get('/picoscope', (req, res) => {
@@ -57,26 +66,40 @@ app.get('/picoscope', (req, res) => {
     console.log('Conectado');
     const data = { data: mqttData };
     res.json(data);
+    res.render('index', {data})
+
   } else {
     res.status(404).json({ error: 'No se han recibido datos MQTT' });
   }
 });
 
-app.use(cors(corsOptions)); 
-app.options('*', cors(corsOptions)); 
+
+
+// Ruta para enviar un mensaje MQTT utilizando POST
+app.post('/conectar', (req, res) => {
+  // Obtener el mensaje desde el cuerpo de la solicitud
+  const mensaje = req.body.mensaje; 
+
+  // Verificar si se proporcionó un mensaje
+  if (!mensaje) {
+    return res.status(400).json({ error: 'Se requiere un mensaje en el cuerpo de la solicitud' });
+  }
+
+  // Publicar el mensaje en el tópico MQTT deseado
+  mqttClient.publish('test/server', mensaje, { qos: 2 }, (error) => {
+    if (!error) {
+      console.log('Mensaje MQTT enviado con éxito:', mensaje);
+      res.json({ mensaje: 'Mensaje MQTT enviado con éxito' });
+    } else {
+      console.error('Error al enviar el mensaje MQTT:', error);
+      res.status(500).json({ error: 'Error al enviar el mensaje MQTT' });
+    }
+  });
+});
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
-
-//PUBLICAR MENSAJE
-// const mensaje = "Ola desde api";
-// mqttClient.publish('/pruebas', mensaje, { qos: 0 }, (error) => {
-//   if (!error) {
-//     console.log('Mensaje MQTT enviado con éxito');
-//   } else {
-//     console.error('Error al enviar el mensaje MQTT:', error);
-//   }
-// });
-
-
 
 app.listen(PORT, () => {
   console.log(`Servidor Node.js escuchando en el puerto ${PORT}`);
